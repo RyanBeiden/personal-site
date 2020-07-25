a33666672eb5e278614b1c162fea227b17e2f5a7
Ryan Beiden <rdbeiden@gmail.com>
2020-07-25 14:15:47 -0500

    wip / changed color theme

--- src/javascripts/components/landingPage/landingPage.js
+++ src/javascripts/components/landingPage/landingPage.js
@@ -9,23 +9,25 @@ let domString = '';
 
 const buildLandingPage = () => {
   domString += `
-    <div class="icons">
-      <div class="socials mt-4">
-        <a href="https://github.com/RyanBeiden" target="_blank"><i class="fab fa-github"></i></a>
+    <div class="landing-container">
+      <div class="icons">
+        <div class="socials mt-4">
+          <a href="https://github.com/RyanBeiden" target="_blank"><i class="fab fa-github"></i></a>
+        </div>
+        <div class="socials">
+          <a href="https://www.instagram.com/ryanbeiden" target="_blank"><i class="fab fa-instagram"></i></a>
+        </div>
       </div>
-      <div class="socials">
-        <a href="https://www.instagram.com/ryanbeiden" target="_blank"><i class="fab fa-instagram"></i></a>
-      </div>
+      <nav>
+        <h1>Ryan Beiden</h1>
+        <h3>Software Developer | Travel Photographer | Culture Enthusiast</h3>
+        <ul class="button-group">
+          <span style="margin: 20px;"><a id="bio"><li>Bio</li></a></span>
+          <span style="margin: 20px;"><a id="tech"><li>Technologies</li></a></span>
+          <span style="margin: 20px;"><a id="proj"><li>Projects</li></a></span>
+        </ul>
+      </nav>
     </div>
-    <nav class="landing-page">
-      <h1>Ryan Beiden</h1>
-      <h3>Software Developer | Travel Photographer | Culture Enthusiast</h3>
-      <ul class="button-group">
-        <span style="margin: 20px;"><a id="bio"><li>Bio</li></a></span>
-        <span style="margin: 20px;"><a id="tech"><li>Technologies</li></a></span>
-        <span style="margin: 20px;"><a id="proj"><li>Projects</li></a></span>
-      </ul>
-    </nav>
   `;
   utils.printToDom('#web-page', domString);
   $('body').on('click', '#proj', projects.buildProjectCards);
