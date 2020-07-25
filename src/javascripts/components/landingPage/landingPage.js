348cb4a3a923b41d6e5d04fcc8269c52415e4651
Ryan Beiden <rdbeiden@gmail.com>
2020-07-25 15:59:35 -0500

    wip / add footer

--- src/javascripts/components/landingPage/landingPage.js
+++ src/javascripts/components/landingPage/landingPage.js
@@ -10,23 +10,21 @@ let domString = '';
 const buildLandingPage = () => {
   domString += `
     <div class="landing-container">
-      <div class="icons">
-        <div class="socials mt-4">
-          <a href="https://github.com/RyanBeiden" target="_blank"><i class="fab fa-github"></i></a>
-        </div>
-        <div class="socials">
-          <a href="https://www.instagram.com/ryanbeiden" target="_blank"><i class="fab fa-instagram"></i></a>
-        </div>
+      <div class="socials mt-4">
+        <a href="https://github.com/RyanBeiden" target="_blank"><i class="fab fa-github"></i></a>
       </div>
       <nav>
         <h1>Ryan Beiden</h1>
         <h3>Software Developer | Travel Photographer | Culture Enthusiast</h3>
         <ul class="button-group">
-          <span style="margin: 20px;"><a id="bio"><li>Bio</li></a></span>
-          <span style="margin: 20px;"><a id="tech"><li>Technologies</li></a></span>
-          <span style="margin: 20px;"><a id="proj"><li>Projects</li></a></span>
+          <a id="bio"><li>Bio</li></a>
+          <a id="tech"><li>Technologies</li></a>
+          <a id="proj"><li>Projects</li></a>
         </ul>
       </nav>
+      <footer>
+        <p>© 2020 Ryan Beiden</p>
+      </footer>
     </div>
   `;
   utils.printToDom('#web-page', domString);
