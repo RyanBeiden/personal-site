4802fb6b6cd00d8f9299d25c6b1fdf75ea69dd4f
Ryan Beiden <rdbeiden@gmail.com>
2020-07-27 17:49:58 -0500

    add new project section with bootstrap accordion

--- src/javascripts/components/projects/projects.js
+++ src/javascripts/components/projects/projects.js
@@ -3,60 +3,54 @@ import projectData from '../../helpers/data/projectData';
 import '../../../styles/main.scss';
 import './projects.scss';
 
-const scrollEvent = (e) => {
-  e.preventDefault();
-  window.scroll({
-    top: 1000,
-    left: 1000,
-    behavior: 'smooth',
-  });
-  $('.arrow').addClass('no-arrow');
-};
-
 const buildProjectCards = () => {
   projectData.getProjects()
     .then((projects) => {
       let domString = `
-        <div class="icons">
-          <div class="arrow">
-            <a id="down-arrow"><i class="fas fa-arrow-down"></i></a>
-          </div>
-          <div class="home mt-4">
-            <a id="go-home"><i class="fas fa-home"></i></a>
-          </div>
-          <div class="socials">
-            <a href="https://github.com/RyanBeiden" target="_blank"><i class="fab fa-github"></i></a>
-          </div>
-          <div class="socials">
-            <a href="https://www.instagram.com/ryanbeiden" target="_blank"><i class="fab fa-instagram"></i></a>
-          </div>
+        <div class="home mt-4">
+          <a id="go-home"><i class="fas fa-home"></i></a>
         </div>
-        <div class="d-flex align-items-center justify-content-center flex-wrap">
+        <div class="accordion container" id="accordionExample">
       `;
       projects.forEach((project) => {
         if (project.available === true) {
           domString += `
-            <div class="projects-page">
-              <h2>${project.title}</h2>
-              <div class="project-container">
-                <div class="description">
-                  <img src="${project.screenshot}">
-                  <p>${project.description}</p>
-                </div>
-                <div class="usedTech">
+            <div class="card">
+              <div class="card-header" id="heading${project.id}">
+                <h5 class="mb-0 d-flex justify-content-center">
+                  <button class="btn drop-btn" type="button" data-toggle="collapse" data-target="#collapse${project.id}" aria-expanded="true" aria-controls="collapse${project.id}">
+                    ${project.title} <i class="fas fa-level-down-alt pl-2"></i>
+                  </button>
+                </h5>
+              </div>
+              <div id="collapse${project.id}" class="collapse" aria-labelledby="heading${project.id}" data-parent="#accordionExample">
+                <div class="card-body">
+                  <div class="projects-page row">
+                    <div class="col">
+                      <img class="container" src="${project.screenshot}">
+                      <div class="d-inline">
+                        <h3>Technologies Used:</h3>
+                        <div class="used-tech d-flex justify-content-center align-items-start">
             `;
           // eslint-disable-next-line no-plusplus
           for (let i = 0; i < project.technologiesUsed.length; i++) {
             domString += `<h4>${project.technologiesUsed[i]}</h4>`;
           }
           domString += `
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col project-data">
+                    <p>${project.description}</p>
+                    <div class="view-links">
+                      <a href="${project.url}" target="_blank">View Project</a>
+                      <a href="${project.githubURL}" target="_blank">View Repository</a>
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div class="d-flex justify-content-center">
-                <a href="${project.url}" target="_blank">View Project</a>
-                <a href="${project.githubURL}" target="_blank">View Repository</a>
-              </div>
             </div>
+          </div>
           `;
         } else;
       });
@@ -65,7 +59,6 @@ const buildProjectCards = () => {
       `;
       utils.printToDom('#web-page', domString);
       $('body').on('click', '#go-home', utils.goHome);
-      $('body').on('click', '#down-arrow', scrollEvent);
     })
     .catch((err) => console.error('get projects broke...', err));
 };
